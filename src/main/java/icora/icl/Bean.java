d8157a7a243a3a9b9f567ad8f9dbe60bd7985e57
khalid-icoral <caidi.khalid@gmail.com>
2015-02-24 10:20:22 +0000

    tete

--- src/main/java/icora/icl/Bean.java
+++ src/main/java/icora/icl/Bean.java
@@ -1,12 +1,5 @@
 package icora.icl;
-//modification pour rien
-//8888888888884444
-//<<<<<<< HEAD
-//////////
-//=======
-//555  
-p
-//>>>>>>> refs/heads/master
+
 public interface Bean {
 
     boolean isABean();
